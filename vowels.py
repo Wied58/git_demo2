865583e251015dd59be5910e8f354a3be3fef7d0
Jeff Wiedemann <jeff@jeffwiedemann.com>
2019-10-21 15:11:03 -0500

    initial commit

---
+++ vowels.py
@@ -0,0 +1,10 @@
+#!/anaconda3/bin/python
+
+vowels = ['a','e','i','o','u']
+word = "Milliways"
+for letter in word:
+   if letter in vowels:
+      print (letter)
+   # end of letter if
+#end of letter in word loop
+
